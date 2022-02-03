5349201324ede39eb180e50a37ccc5cd44c779b2
Alex <asenges400@gmail.com>
2022-02-03 14:26:28 -0300

    Low level programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: 0
+ *
+ * Description:
+ *
+ */
+
+int main(void)
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+
+}
