34284ee0772bc204f8af717df89c7418b2ce574b
Alex <asenges400@gmail.com>
2022-02-04 12:34:32 -0300

    Low level programmingi if... else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ else if (lastDigit == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 }
-else if (lastDigit > 0 && lastDigit < 6 || lastDigit < 0)
+else if ((lastDigit > 0 && lastDigit < 6) || lastDigit < 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 }
