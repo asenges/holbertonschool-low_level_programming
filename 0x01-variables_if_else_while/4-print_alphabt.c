fe5d38952ecd469cf0dd8cffe76516c3bafef8c2
Alex <asenges400@gmail.com>
2022-02-04 12:40:29 -0300

    Low level programmingi if... else

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: 0
+ *
+ * Description:
+ *
+ */
+
+int main(void)
+{
+
+char ch = 'a';
+char sp = '\n';
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'e' || ch != 'q')
+{
+putchar(ch);
+}
+}
+
+putchar(sp);
+return (0);
+
+}
