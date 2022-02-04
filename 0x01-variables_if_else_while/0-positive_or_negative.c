5ed9a22c3d57a15e73090cb1dfceb9ee747cdc02
Alex <asenges400@gmail.com>
2022-02-04 10:29:19 -0300

    Low level programmingi if... else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,23 +12,23 @@
 
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
-	{
-		printf("is positive");
-	}
-	else if (n == 0)
-	{
-		printf("is zero");
-	}
-	else
-	{
-		printf("is negative");
-	}
+if (n > 0)
+{
+printf("is positive");
+}
+else if (n == 0)
+{
+printf("is zero");
+}
+else
+{
+printf("is negative");
+}
 
-	return (0);
+return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	lastDigit = num % 10;
+	lastDigit = n % 10;
 
 	if (lastDigit > 5)
 	{
