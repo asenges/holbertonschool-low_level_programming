2eea63b0e650bd3f6c15cab89f9f048914009b87
Alex <asenges400@gmail.com>
2022-02-04 13:16:14 -0300

    Low level programmingi if... else

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: 0
+ *
+ * Description:
+ *
+ */
+
+int main(void)
+{
+
+char ch = 'z';
+char sp = '\n';
+
+for (ch = 'z'; ch >= 'a'; ch--)
+putchar(ch);
+
+putchar(sp);
+return (0);
+
+}
