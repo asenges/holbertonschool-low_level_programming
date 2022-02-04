01d7e03dca62426c5642b26328ac3c5241318ef0
Alex <asenges400@gmail.com>
2022-02-04 14:03:10 -0300

    Low level programmingi if... else

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: 0
+ *
+ * Description:
+ *
+ */
+
+int main(void)
+{
+
+int n = 0;
+
+for (n = 0; n < 10; n++)
+{
+putchar(n + '0');
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+return (0);
+
+}
