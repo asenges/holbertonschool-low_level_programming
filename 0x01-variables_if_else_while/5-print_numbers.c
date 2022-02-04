ff4250609c0e8cc0cbe6e18f9bec137d3b2fc541
Alex <asenges400@gmail.com>
2022-02-04 12:55:53 -0300

    Low level programmingi if... else

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: 0
+ *
+ * Description:
+ *
+ */
+
+int main(void)
+{
+
+int n = 0;
+/*char sp = '\n';*/
+
+for (n = 0; n < 10; n++)
+printf("%d", n);
+
+/*printf(sp);*/
+return (0);
+
+}
