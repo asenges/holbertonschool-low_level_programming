3c719e1a9f544a318e13b37b2e4646c2801b8952
Alex <asenges400@gmail.com>
2022-02-07 12:53:35 -0300

    Low level programming Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,26 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
- * print_alphabet - funtion to call _putchar
- * Return: 0
- *
- * Description:
- * Author: Alex Senges
- * Date: Feb 7, 2022
- */
-
-void print_alphabet(void)
-{
-	char c;
-
-	for (c = 'a' ; c <= 'z'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
-
-}
 
 /**
  * main - program to print the alphabet
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - funtion to call _putchar
+ * Return: 0
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Feb 7, 2022
+ */
+
+void print_alphabet(void)
+{
+        char c;
+
+        for (c = 'a' ; c <= 'z'; c++)
+        {
+        _putchar(c);
+        }
+        _putchar('\n');
+
+}
+
