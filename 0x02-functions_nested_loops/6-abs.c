c5a2d78929c83391ed43d0a1d2b71959b8f6e4cd
Alex <asenges400@gmail.com>
2022-02-07 14:08:36 -0300

    Low level programming Functions, nested loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -17,17 +17,14 @@ int _abs(int n)
 
 	if (n > 0)
 	{
-		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		_putchar('-');
 		return (-1);
 	}
 
