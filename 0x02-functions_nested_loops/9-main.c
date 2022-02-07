e49e4e3af78f64e5cbdac3143d11f11b52d52911
Alex <asenges400@gmail.com>
2022-02-07 19:54:47 -0300

    Low level programming Functions, nested loops

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * main - program to print 9 times table
+ * Return: 0
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Feb 7, 2022
+ */
+
+int main(void)
+{
+
+
+times_table();
+return (0);
+
+
+}
+
+
+
+
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * times_table - function to print 9 times table
+ * Return: void
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Feb 7, 2022
+ */
+
+void times_table(void)
+{
+
+int i, j, k;
+
+i = 0;
+while (i < 10)
+{
+	j = 0;
+	while (j < 10)
+	{
+		k = i *j;
+
+		if ((k / 10) + '0' != '0')
+		{
+			_putchar((k / 10) + '0');
+		}
+
+		_putchar((k % 10) + '0');
+
+		if (j != 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+
+		j++;
+	}
+	_putchar('\n');
+	i++;
+
+}
+
+}
+
