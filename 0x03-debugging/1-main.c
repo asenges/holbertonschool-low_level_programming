9f8fc14fe9c129122f7efca1dfe38898bc3d917d
Alex <asenges400@gmail.com>
2022-02-08 19:33:11 -0300

    Low level programming Functions, nested loops

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,13 +7,10 @@
 
 int main(void)
 {
-
 int i;
 
 printf("Infinite loop incoming :(\n");
-
 i = 0;
-
 /*
  * * while (i < 10)
  *{
@@ -22,8 +19,6 @@ putchar(i);
 /*
  * }
  */
-
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
