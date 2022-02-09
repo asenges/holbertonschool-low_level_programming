4f7f456b9929ca1144b96dfd220cc30bb89f4319
Alex <asenges400@gmail.com>
2022-02-09 14:26:43 -0300

    Low level programming - 0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,26 +15,34 @@ int main(void)
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
+			
 			if (i != 100)
 				printf(" ");
+				
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
+			
 			if (i != 100)
 				printf(" ");
+				 
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
+			
 			if (i != 100)
 				printf(" ");
+				
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
+			
 			if (i != 100)
 				printf(" ");
+				
 		}
 	}
 	printf("\n");
