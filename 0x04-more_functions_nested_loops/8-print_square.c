838184316123ee58ddd95f2d5b9f50bbe8f94268
Alex <asenges400@gmail.com>
2022-02-09 13:41:51 -0300

    Low level programming - 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_square - function to print a square
+ * Return: void
+ * @size: int
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Feb 9, 2022
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
