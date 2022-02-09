e86760058fc991be526de3756d7d9a69702cdfac
Alex <asenges400@gmail.com>
2022-02-09 10:50:06 -0300

    Low level programming - 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * mul - function to multiply two integers
+ * Return: int
+ * @a: int
+ * @b: int
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Feb 9, 2022
+ */
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+}
+
