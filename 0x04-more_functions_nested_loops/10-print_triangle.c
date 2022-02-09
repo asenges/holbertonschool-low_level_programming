a480c46607bc1a3a62bbf2b02758bb0177e3d9aa
Alex <asenges400@gmail.com>
2022-02-09 14:58:32 -0300

    Low level programming - 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_triangle - function to print a triangle
+ * Return: void
+ * @size: int
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Feb 9, 2022
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+
+			for (j = size; j >= 1; j--)
+				if (i < j)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+
+
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
