765c90d4d70efc81a38bd816a04ef63290f2472d
Alex <asenges400@gmail.com>
2022-02-09 10:57:23 -0300

    Low level programming - 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_numbers - function to print numbers from 0-9
+ * Return: void
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Feb 9, 2022
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+}
