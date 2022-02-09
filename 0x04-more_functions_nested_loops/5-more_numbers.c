e54c4a7d609b2b827f1fe065b8d8f2c83e319deb
Alex <asenges400@gmail.com>
2022-02-09 11:22:13 -0300

    Low level programming - 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * more_numbers - function to print more numbers
+ * Return: void
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Feb 9, 2022
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if ((j / 10) != 0)
+			_putchar((j / 10) + '0');
+
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
