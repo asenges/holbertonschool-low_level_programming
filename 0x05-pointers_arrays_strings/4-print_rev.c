48beeaf65fbb7b72d2ebd6e51b67c844ffce24c7
Alex <asenges400@gmail.com>
2022-02-11 16:24:59 -0300

    Low level programming - 0x05 Pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - function to print a str in reverse
+ * Return: void
+ * @s: s pointer
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Feb 11, 2022
+ */
+
+void print_rev(char *s)
+{
+	int len, i;
+
+	len = strlen(s);
+
+	for (i = len; i > 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
