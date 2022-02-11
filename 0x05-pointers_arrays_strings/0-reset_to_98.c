39dec303e60e532bd1649e2cd99c05293200dbf0
Alex <asenges400@gmail.com>
2022-02-11 14:50:37 -0300

    Low level programming - 0x05 Pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - function to receive a pointer
+ * Return: void
+ * @n: int pointer
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Feb 11, 2022
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+These are the files for project "0x05-Pointers"
