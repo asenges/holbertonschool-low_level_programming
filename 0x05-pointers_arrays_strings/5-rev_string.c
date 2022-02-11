8b147f415e37e7086a326beae1413bbdc37d9341
Alex <asenges400@gmail.com>
2022-02-11 17:57:17 -0300

    Low level programming - 0x05 Pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -17,17 +17,17 @@ void rev_string(char *s)
 	int i, j;
 	int len = strlen(s);
 	char rev_str[999];
-	
+
 	j = 0;
 	for (i = len - 1; i >= 0; i--)
 	{
 		rev_str[j] = s[i];
 		j++;
 	}
-	
+
 	i = 0;
 	for (i = 0; i < len; i++)
 	{
 		s[i] = rev_str[i];
-	}	
+	}
 }
