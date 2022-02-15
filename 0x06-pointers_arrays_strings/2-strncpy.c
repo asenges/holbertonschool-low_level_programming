3763ea350216170cce699891af8587e95bc29748
Alex <asenges400@gmail.com>
2022-02-15 11:26:33 -0300

    Low level programming - 0x06 More Pointers

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - function to copy one string to a destination
+ * Return: char ptr
+ * @dest: char ptr
+ * @src: char ptr
+ * @n: int var
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Feb 11, 2022
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	size_t j, destlen, k; 
+	destlen = strlen(dest);
+	k = n;
+
+	/* Clean Buffe  */
+	if (k > destlen - k)
+	{
+		for (j = 0; j < destlen; j++)
+			dest[j] = '\0';
+	}
+	
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+
+
+
+	return (dest);
+}
