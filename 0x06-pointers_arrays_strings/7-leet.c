4854ced9d3185c58f035c2cc5e94efafda204562
Alex <asenges400@gmail.com>
2022-02-16 10:25:30 -0300

    Low level programming - 0x06 More Pointers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * leet - function to upp all characters in array
+ * Return: char
+ * @s: char ptr
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Feb 11, 2022
+ */
+
+char *leet(char *s)
+{
+	size_t i, j;
+	char letter[] = {97, 101, 111, 116, 108};
+	char number[] = {52, 51, 48, 55, 49};
+
+	for (i = 0; i < strlen(s); i++)
+	{
+		for (j = 0; j < strlen(letter); j++)
+			if (s[i] == letter[j] && s[i] == letter[i] + 32)
+				s[i] = number[j];
+	}
+
+	return (s);
+}
