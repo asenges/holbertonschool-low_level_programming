4d5d05a1cbe8ec617b1c953ba204dbc7966c4fb8
Alex <asenges400@gmail.com>
2022-02-16 09:08:15 -0300

    Low level programming - 0x06 More Pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,22 +17,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	size_t j, destlen, k;
-
-	destlen = strlen(dest);
-	k = n;
-
-	/* Clean Buffer  */
-	if (k > destlen - k)
-	{
-		for (j = 0; j < destlen; j++)
-			dest[j] = '\0';
-	}
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
-
+	for (; i < n; i++)
+		dest[i] = '\0';
 
 
 	return (dest);
