f5e671f1098deeb8ced405792b7fa8a6aa6c6633
Alex <asenges400@gmail.com>
2022-02-21 12:40:48 -0300

    Low level programming - 0x06 More Pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - function
+ * Return: char pointer
+ * @s: char pointer
+ * @c: char
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Feb 21, 2022
+ */
+
+char *_strchr(char *s, char c)
+{
+	size_t i;
+	char *pos;
+
+	pos = s;
+
+	for (i = 0; i < strlen(s); i++)
+	{
+		if (s[i] == c)
+		{
+			pos = s + i;
+			return (pos);
+		}
+		
+	}
+	return (NULL);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# holbertonschool-low_level_programming
+Holberton C Programming Repo
+0x07. C - More pointers, arrays and strings
