ae97fafa7fc8ca9792b992eed2f0ccbb3038e13d
Alex <asenges400@gmail.com>
2022-02-22 09:02:49 -0300

    Low level programming - 0x06 More Pointers

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -15,10 +15,22 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
-	
-	for (i = 0; i < 8; i++)
+	int i = 0;
+	int j = 0;
+
+	for (; i < 8; i++)
 	{
-		_putchar(a[i]);
+		for (j = 0; j < 8; j++)
+		{
+			if (j != 7)
+			{
+				_putchar(a[i][j]);
+			}
+			else
+			{
+				_putchar(a[i][j]);
+				_putchar(10);
+			}
+		}
 	}
 }
