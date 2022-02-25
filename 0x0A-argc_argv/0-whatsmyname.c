d192be117e60519994929095ec2e01abc6f81ffe
Alex <asenges400@gmail.com>
2022-02-25 09:14:35 -0300

    Low level programming - 0x0A-argc-argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int argc __attribute__((__unused__)), char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
