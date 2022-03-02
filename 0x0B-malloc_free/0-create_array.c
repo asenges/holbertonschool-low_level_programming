c4d71fd08dd509f9027258207553cbb8fd311e41
Alex <asenges400@gmail.com>
2022-03-02 09:58:19 -0300

    Low level programming - 0x0B-Malloc / Free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * create_array - function
+ * Return: char pointer
+ * @size: int
+ * @c: char
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Mar 2, 2022
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size != 0)
+	{
+		char *a = malloc(sizeof(char) * size);
+		size_t i;
+
+		for (i = 0; i < size; i++)
+		{
+			a[i] = c;
+		}
+
+		if (a != NULL)
+		{
+			return (a);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
+}
