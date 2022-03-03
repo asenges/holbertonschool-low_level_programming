61a1d819b7619a719372849de20f7685cff3a55e
Alex <asenges400@gmail.com>
2022-03-03 19:42:29 -0300

    Low level programming - 0x0B-malloc_free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function
+ * Return: 0
+ * @b: unsigned int
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Mar 2, 2022
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	if (b != 0)
+	{
+		void *p = malloc(b);
+
+		if (p == NULL)
+			exit(EXIT_FAILURE);
+		return (p);
+	}
+	else
+	{
+		exit(EXIT_FAILURE);
+	}
+}
