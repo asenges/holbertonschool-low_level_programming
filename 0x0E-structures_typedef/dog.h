0370028e4d2977d050a12ccf86730a4f40824c5e
Alex <asenges400@gmail.com>
2022-03-08 15:54:34 -0300

    Low level programming - 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+/**
+ * struct dog - struct
+ * @name: char
+ * @age: float
+ * @owner: char
+ *
+ * Description:
+ * Author: Alex Senges
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
