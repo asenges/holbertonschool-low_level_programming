30aab7e8f895bf37ae3159f527f68d74f3877384
Alex <asenges400@gmail.com>
2022-03-08 19:32:47 -0300

    Low level programming - 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - function
+ * Return: void
+ * @d: struct pointer
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Mar 08, 2022
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)");
+		else
+			printf("Name: %s\n", d->name);
+
+		if (d->age == 0.0)
+			printf("Age: (nil)");
+		else
+			printf("Age: %f\n", d->age);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
+
