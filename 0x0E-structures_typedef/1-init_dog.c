b617ef49ae1e369a64105505c6feb1f79df79eaa
Alex <asenges400@gmail.com>
2022-03-08 19:03:23 -0300

    Low level programming - 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - function
+ * Return: void
+ * @dog: struct
+ * @name: char
+ * @age: float
+ * @owner: char
+ *
+ * Description:
+ * Author: Alex Senges
+ * Date: Mar 08, 2022
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;	
+}
+
