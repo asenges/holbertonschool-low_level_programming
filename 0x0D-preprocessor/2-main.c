ea82c0bcf3a5242d1cc390e1661b94aedff491d5
Alex <asenges400@gmail.com>
2022-03-08 15:06:01 -0300

    Low level programming - 0x0D-preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,13 +3,11 @@
 /**
  * main - check the code
  * Return: Always 0.
- * @argc: int
- * @argv: char
  *
- * Description: 
+ * Description:
  * Author: Alex Senges
  */
-int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
