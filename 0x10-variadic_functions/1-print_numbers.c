cda1419787dd1e73916b64fa55ac4d9420063b1d
Alex <asenges400@gmail.com>
2022-03-10 21:48:51 -0300

    Low level programming - 0x10-variadic_functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,7 +24,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if (i < n - 1)
+
+		if (i == n - 1)
+			break;
+
+		if (separator != NULL)
 			printf("%s", separator);
 	}
 	va_end(ap);
