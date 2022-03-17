de69728ff63cddccb1b626bea90b9e554eb20af2
Alex <asenges400@gmail.com>
2022-03-17 15:07:27 -0300

    Low level programming - 0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+/**
+ * add_node_end - node add end
+ * @head: list_t ptr ptr
+ * @str: char ptr
+ * Return: size_int
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
