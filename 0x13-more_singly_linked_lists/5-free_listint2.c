d5693e19904c8933fdc4d5e50baf56ad26d1c5c7
Alex <asenges400@gmail.com>
2022-03-22 11:46:59 -0300

    Low level programming - 0x12-singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+/**
+ * free_listint - free list
+ * @head: list_t ptr
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+}
