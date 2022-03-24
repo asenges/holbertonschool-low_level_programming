c1fb51f5ebfc1a21f80c7a36df3ef9422e0b0ee4
Alex <asenges400@gmail.com>
2022-03-24 18:55:16 -0300

    Low level programming - 0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * get_bit - get_bit
+ * @n: unsigned long int
+ * @index: unsigned int
+ * Return: int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{	
+	if (index > 33)
+		return (-1);
+
+	n = n >> index;
+	if ((n & 1) == 0)
+		return (0);
+	else
+		return (1);
+}
