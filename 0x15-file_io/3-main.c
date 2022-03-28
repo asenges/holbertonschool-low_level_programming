accb46294f5cc65d445486178616e4c8a33e63c4
Alex <asenges400@gmail.com>
2022-03-28 20:56:15 -0300

    Low level programming - 0x15--file_io

---
+++ 0x15-file_io/3-main.c
@@ -0,0 +1,61 @@
+#include "main.h"
+/**
+ * closing - close function
+ * @n: as a file handler
+ * Return: int
+ */
+void closing(int fd)
+{
+	int res = 0;
+
+	res = close(fd);
+	if (res == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - copy a file to another
+ * @argc: source file
+ * @argv: destine file
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	static char buf[1024];
+	int sf = 0, sd = 0, flen = 1024, dlen = 0;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	sf = open(argv[1], O_RDONLY);
+	if (sf == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	umask(0);
+	sd = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	if (sd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while (flen == 1024)
+	{
+		flen = read(sf, buf, 1024);
+		dlen = write(sd, buf, flen);
+		if (dlen == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	closing(sf);
+	closing(sd);
+	return (0);
+}
