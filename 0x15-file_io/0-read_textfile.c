f0444505421a2834e3fc837c6348b674718e44e9
Alex <asenges400@gmail.com>
2022-03-28 14:30:57 -0300

    Low level programming - 0x15--file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,26 +17,30 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!buf)
 		return (0);
 
-	if (filename == NULL){
+	if (filename == NULL)
+	{
 		free(buf);
 		return (0);
 	}
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1){
+	if (fd == -1)
+	{
 		free(buf);
 		return (0);
 	}
 
 	rres = read(fd, buf, letters);
-	if (rres == -1){
+	if (rres == -1)
+	{
 		close(fd);
 		free(buf);
 		return (0);
 	}
 
 	wres = write(STDIN_FILENO, buf, rres);
-	if (wres == -1 || rres != wres){
+	if (wres == -1 || rres != wres)
+	{
 		close(fd);
 		free(buf);
 		return (0);
