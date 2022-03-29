4fc5d47be4964dcdcae59c351446f0c7ccf4f512
Alex <asenges400@gmail.com>
2022-03-28 22:01:04 -0300

    Low level programming - 0x15--file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
 		flen = read(sf, buf, 1024);
 		if (flen == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 		dlen = write(sd, buf, flen);
