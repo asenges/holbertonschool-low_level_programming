dec86a940261e7447d82b2e25ce5f5312f8e70a4
Alex <asenges400@gmail.com>
2022-05-05 17:49:16 -0300

    Low level programming - 0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,23 @@
 #include "lists.h"
+/**
+ * dlistint_len_helper - returns de number of elements in the list
+ * Return: size_t
+ * @h: const dlistint_t ptr
+ */
+size_t dlistint_len_helper(const dlistint_t *h)
+{
+	int n = 0;
 
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
 /**
  * insert_dnodeint_at_index - insert an elements to the list at index
  * Return: dlistint_t
@@ -11,13 +29,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new = NULL, *head = NULL;
 	unsigned int i = 0;
+	size_t list_len = 0;
 
 	new = malloc(sizeof(dlistint_t));
 	if (!h || !new)
 		return (NULL);
 
+	list_len = dlistint_len_helper(head);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
+	if (idx == list_len)
+		return (add_dnodeint_end(h, n));
 
 	new->n = n;
 	new->prev = NULL;
