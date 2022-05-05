9d7e691e29503abd5fe5811cb0cc26eeec576841
Alex <asenges400@gmail.com>
2022-05-05 17:39:04 -0300

    Low level programming - 0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	new = malloc(sizeof(dlistint_t));
-	if (!new)
+	if (!h || !new)
 		return (NULL);
 
 	if (idx == 0)
