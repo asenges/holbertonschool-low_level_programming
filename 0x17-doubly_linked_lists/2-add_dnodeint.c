ff3ddc50f4e05aac13751d9c2ebc4c115bbcd1de
Alex <asenges400@gmail.com>
2022-05-05 08:42:22 -0300

    Low level programming - 0x17. C - Doubly linked lists

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add an elements to the list
+ * Return: void
+ * @head: const dlistint_t double ptr
+ * @n: const int
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = NULL;
+
+	if (!head)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	if (!*head)
+		new->next = NULL;
+	else
+		new->next = *head;
+
+	new->n = n;
+	new->prev = NULL;
+
+	/*(*head)->prev = new;*/
+	*head = new;
+
+	return (*head);
+}
