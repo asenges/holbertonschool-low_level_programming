7ec80a97a10f219d0769c472dbad8caa47cd5791
Alex <asenges400@gmail.com>
2022-05-05 07:02:16 -0300

    Low level programming - 0x17. C - Doubly linked lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print elements in the list
+ * Return: size_t
+ * @h: const dlistint_t ptr
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,2 @@
+# holbertonschool-python
+0x17. C - Doubly linked lists
