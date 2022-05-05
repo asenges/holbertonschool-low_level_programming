c89d69f145d82f54cc76a9325ca502ecb966f8ba
Alex <asenges400@gmail.com>
2022-05-05 12:51:35 -0300

    Low level programming - 0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,25 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - returns de number of elements in the list
+ * Return: size_t
+ * @h: const dlistint_t ptr
+ */
+size_t dlistint_len_helper(const dlistint_t *h)
+{
+	int n = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
+/**
  * get_dnodeint_at_index - returns the element in ntx index of the list
  * Return: dlistint_t
  * @head: dlistint_t ptr
@@ -9,6 +28,12 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
+	size_t list_len = 0;
+	const dlistint_t *h = head;
+
+	list_len = dlistint_len_helper(h);
+	if (index >= list_len)
+		return (NULL);
 
 	if (!head)
 		return (NULL);
