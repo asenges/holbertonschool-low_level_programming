d7223640a6d60d5b525a7fcc3ef3a75affe5761f
Alex <asenges400@gmail.com>
2022-05-26 20:19:10 -0300

    Low level programming - 0x1A. C - Hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - This function creates a hash table
+ *
+ * Return: hash_table_t
+ * @size: unsigned long int
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+}
