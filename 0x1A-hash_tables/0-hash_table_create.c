380038be56382fb337c6880a8b4b59eb77286a2d
Alex <asenges400@gmail.com>
2022-05-26 23:38:47 -0300

    Low level programming - 0x1A. C - Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,22 +13,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (!size || size == 0)
 		return (NULL);
-
 	new_hasht = malloc(sizeof(hash_table_t));
 	if (!new_hasht)
 		return (NULL);
-
 	new_hasht->array = malloc(sizeof(hash_node_t *) * size);
 	if (!new_hasht->array)
 	{
 		free(new_hasht);
 		return (NULL);
 	}
-
 	new_hasht->size = size;
 	for (i = 0; i < size; i++)
-	{
 		new_hasht->array[i] = NULL;
-	}
 	return (new_hasht);
 }
